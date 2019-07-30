0ffa6b5eac8db56645beb0385a02073c34591bfc
RyanDBarnett <conner2468@gmail.com>
2019-07-29 21:52:40 -0600

    Create Synonyms and Synonym components

---
+++ src/components/Synonym.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>{{synonym}}</li>
+</template>
+
+<script>
+
+export default {
+  name: 'Synonym',
+  props: ['synonym']
+}
+</script>
---
+++ src/components/Synonyms.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul v-for="(synonym, index) in synonyms">
+      <Synonym v-bind:key="index" v-bind:synonym="synonym" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Synonym from './Synonym';
+
+export default {
+  name: 'Synonyms',
+  components: {
+    Synonym
+  },
+  props: ['synonyms']
+}
+</script>
