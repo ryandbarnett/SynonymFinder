685053d0d8261e5495cebfc131cb97f03e4f8865
RyanDBarnett <conner2468@gmail.com>
2019-07-29 21:52:11 -0600

    Create Results and Result components

---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>{{result.shortdef[0]}}</h3>
+    <Synonyms v-bind:synonyms="result.meta.syns[0]" />
+  </div>
+</template>
+
+<script>
+import Synonyms from './Synonyms';
+
+export default {
+  name: 'Result',
+  components: {
+    Synonyms
+  },
+  props: ['result']
+}
+</script>
---
+++ src/components/Results.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Results for "{{results[0].meta.id}}"</h2>
+    <div v-for="result in results">
+      <Result v-bind:key="result.meta.uuid" v-bind:result="result" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Result from './Result';
+
+export default {
+  name: 'Results',
+  components: {
+    Result
+  },
+  props: ['results']
+}
+</script>
