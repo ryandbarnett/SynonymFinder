2d53526d609aa1e722e7871c18f386f8d0befc1f
RyanDBarnett <conner2468@gmail.com>
2019-07-30 19:56:27 -0600

    Create updateValue and submit methods for Form and bind the value prop

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,21 @@
 <template>
-  <form>
-    <input />
+  <form v-on:submit.prevent="submit()">
+    <input v-bind:value="value" v-on:input="updateValue($event.target.value)"/>
+    <input type="submit" />
   </form>
 </template>
 
 <script>
 export default {
-  name: 'Form'
+  name: 'Form',
+  props: ['value'],
+  methods: {
+    updateValue(value) {
+      this.$emit('input', value);
+    },
+    submit() {
+      this.$emit('submit');
+    }
+  }
 }
 </script>
