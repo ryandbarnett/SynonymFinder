e748431e51416ac571a64da1cba316077783f80a
RyanDBarnett <conner2468@gmail.com>
2019-07-31 11:03:30 -0600

    Style app and change Header component to Title

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <Header />
-    <Form v-model="value" v-on:submit="getSynonyms(value)" />
-    <div v-if="results.length">
+    <header>
+      <Title />
+      <Form v-model="value" v-on:submit="getSynonyms(value)" />
+    </header>
+    <main class="Results-container" v-if="results.length">
       <Results v-bind:results="results" v-on:click="handleClick"/>
-    </div>
-    <h2 v-if="error">{{ error }}</h2>
+    </main>
+    <h2 class="error" v-if="error">{{ error }}</h2>
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
+import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
 import fetchData from './utils/fetchData';
@@ -25,7 +27,7 @@ export default {
     }
   },
   components: {
-    Header,
+    Title,
     Form,
     Results
   },
@@ -39,7 +41,7 @@ export default {
           this.error = '';
         } else {
           this.results = [];
-          this.error = 'No synonyms found :(';
+          this.error = `No synonyms found for "${value}"`;
         }
       });
     },
@@ -52,14 +54,44 @@ export default {
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-image: url(./assets/arches.png);
+    background-color: #F1FAEE;
+    height: 100vh;
+  }
+
+  .Results-container {
+    width: 100%;
+    border-top: 2px solid #1D3557;
+    background: #457B9D;
+    margin: 20px 0;
+    padding: 20px 40px;
+  }
+
+  header {
+    background: #457B9D;
+    border: 2px solid #1D3557;
+    border-radius: 10px;
+    margin: 30px 0;
+    padding: 30px;
+    box-shadow: 0px 2px 3px #1D3557;
+  }
+
+  .error {
+    color: #E63946;
+  }
 </style>
