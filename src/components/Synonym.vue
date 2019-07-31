c8aaa5059536e530124081f04afc7bd35d1839f8
RyanDBarnett <conner2468@gmail.com>
2019-07-31 11:08:02 -0600

    Style Synonym component

--- src/components/Synonym.vue
+++ src/components/Synonym.vue
@@ -1,5 +1,5 @@
 <template>
-  <li v-on:click="click(synonym)">{{synonym}}</li>
+  <li class="Synonym" v-on:click="click(synonym)">{{synonym}}</li>
 </template>
 
 <script>
@@ -14,3 +14,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .Synonym {
+    padding: 10px 15px;
+    margin: 10px;
+    border-radius: 10px;
+    background: #457B9D;
+    box-shadow: 2px 3px 1px #1D3557;
+    text-transform: uppercase;
+    cursor: pointer;
+    color: #F1FAEE;
+    font-weight: bold;
+    border: 2px solid transparent;
+    transition: .25s;
+  }
+
+  .Synonym:hover {
+    color: #457B9D;
+    background: #F1FAEE;
+    border: 2px solid #457B9D;
+    box-shadow: 1px 2px 1px #1D3557;
+  }
+</style>
