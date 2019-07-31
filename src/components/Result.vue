e680644d4bddb20c721660ebf7b6621a7e49133b
RyanDBarnett <conner2468@gmail.com>
2019-07-31 11:07:04 -0600

    Style Result component

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h3>{{result.shortdef[0]}}</h3>
+  <div class="Result">
+    <div class="Result-header">
+      <h3>Definition: {{result.shortdef[0]}}</h3>
+      <p><i>{{result.fl}}</i></p>
+      <h3>Synonyms:</h3>
+      <hr>
+    </div>
     <Synonyms v-bind:synonyms="result.meta.syns[0]" v-on:click="emitClick" />
   </div>
 </template>
@@ -21,3 +26,24 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .Result {
+    color: #E63946;
+    background: #F7FFF7;
+    border: 2px solid #1D3557;
+    border-radius: 10px;
+    margin: 30px 0;
+    padding: 30px;
+    box-shadow: 0px 2px 3px #1D3557;
+  }
+
+  .Result-header {
+    margin: 0 10px;
+  }
+
+  h4, hr {
+    margin-bottom: 20px;
+  }
+
+</style>
