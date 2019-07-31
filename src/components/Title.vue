62a7c15280ac5bca32640c3e711ea5924f393039
RyanDBarnett <conner2468@gmail.com>
2019-07-31 11:06:24 -0600

    Change Header component to Title and style

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Find a Synonym!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: #F1FAEE;
+    text-shadow: 0px 2px 2px #1D3557;
+    text-transform: uppercase;
+  }
+</style>
